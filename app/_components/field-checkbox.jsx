54fa3cd35981af7794f33481624536efc3df43fe
teerakan-otk <teerakan.otk@outlook.com>
2026-03-10 17:56:48 +0700

    refactor: restructure project for better scalability

--- app/_components/field-checkbox.jsx
+++ app/_components/field-checkbox.jsx
@@ -10,7 +10,20 @@ import {
   FieldLegend,
 } from "@/components/ui/field";
 import { Checkbox } from "@/components/ui/checkbox";
-import { optionItems } from "@/constants/options";
+
+const optionItems = [
+  { id: "uppercase", title: "Include Uppercase Letters (A–Z)" },
+  { id: "lowercase", title: "Include Lowercase Letters (a–z)" },
+  { id: "number", title: "Include Numbers (0–9)" },
+  { id: "symbol", title: "Include Symbols (@, #, etc.)" },
+  { id: "beginWithLetter", title: "Ensure Password Starts With a Letter" },
+  { id: "excludeDuplicate", title: "Disallow Duplicate Characters" },
+  {
+    id: "excludeSimilar",
+    title: "Exclude Similar-Looking Characters (i, I, l, 1, o, O, 0)",
+  },
+  { id: "saveSetting", title: "Save Setting" },
+];
 
 export function FieldCheckbox({ title, name, control }) {
   return (
