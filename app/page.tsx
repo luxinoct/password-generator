aa26607795288f8a0a368b9d0b93edf08cd8fca3
teerakan-otk <teerakan.otk@outlook.com>
2026-05-24 12:42:13 +0700

    feat: implement home page with password generator form

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { GeneratePasswordForm } from "@/components/generate-password-form";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col flex-1 p-6">
-      <GeneratePasswordForm />
+    <div className="flex justify-center p-6">
+      <div className="w-full max-w-6xl">
+        <GeneratePasswordForm />
+      </div>
     </div>
   );
 }
